d3cef696e4cf507ec4eaa227ca7699695b4a7c09
rodrigo rodriguez <rodrigo.rodriguez@rabit.cl>
2022-08-13 01:21:55 -0400

    Finalización del ejercicio tema 3.

--- ejercicios-tema-3/src/Main.java
+++ ejercicios-tema-3/src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
+    /*
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int resultado=0;
+        resultado=sumatoria(1,2,3);
+        System.out.println(resultado);
     }
+    public static int sumatoria(int a, int b, int c){
+        return a+b+c;
+    }
+    */
+
+    public static void main(String[] args) {
+        Coche miCoche = new Coche();
+
+        miCoche.sumarPuerta();
+
+        System.out.println(miCoche.puertas);
+    }
+}
+
+class Coche{
+    public int puertas=0;
+
+    public void sumarPuerta(){
+        this.puertas++;
+    }
+
 }
